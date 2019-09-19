268bf447c71345ae935f7821b54ead10fb1f7afa
Mike McGrath <mike@dopefruit.com>
2019-09-19 17:34:35 -0400

    add example code for lambda function

--- aws.js
+++ aws.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const LambdaOptions = {
 	FunctionName: 'php-function-hello',
 	Description: "first try using the runtime api to create the layers",
@@ -13,6 +15,26 @@ const LambdaOptions = {
 	]
 }
 
+const params = {
+	Code: { /* required */
+		S3Bucket: 'STRING_VALUE',
+		S3Key: 'STRING_VALUE',
+		S3ObjectVersion: 'STRING_VALUE',
+		ZipFile: Buffer.from('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */
+	},
+	FunctionName: 'STRING_VALUE', /* required */
+	Handler: 'STRING_VALUE', /* required */
+	Role: 'STRING_VALUE', /* required */
+	Runtime: 'provided',
+	Description: 'STRING_VALUE',
+	Layers: [
+		'arn:aws:lambda:us-east-1:284327142304:layer:php-73-runtime',
+		'arn:aws:lambda:us-east-1:284327142304:layer:php-73-runtime-vendor'
+	],
+	MemorySize: 128,
+	Publish: true,
+	Timeout: 900
+};
 
 const S3Options = {
 	name: 'phplayer-bucket-mmcgrath',
--- serverless.js
+++ serverless.js
@@ -1,8 +1,11 @@
 const { Component } = require('@serverless/core')
 const zip = require('bestzip')
+const AWS = require('aws-sdk');
+
 
 class PHPFunction extends Component {
-  static DEFAULT_S3KEY = './build.zip';
+  static DEFAULT_S3KEY = 'build.zip';
+  static DEFAULT_S3BUCKET = 'phplayer-bucket-mmcgrath';
 
   async default(inputs = {}) {
     //If passed in from serverless.yml, set it
@@ -20,11 +23,48 @@ class PHPFunction extends Component {
     await this.save();
 
 
+    const bucket = await this.load('@serverless/aws-s3');
+
+    await bucket({
+      accelerated: true,
+      name: 'phplayer-bucket-mmcgrath'
+    })
+
+    await bucket.upload({ file: 'build.zip' })
+
+    /** Lambda Code Starts */
+    const params = {
+      Code: {
+        S3Bucket: this.constructor.DEFAULT_S3BUCKET,
+        S3Key: this.state.S3KEY,
+      },
+      FunctionName: 'php-layer-function',
+      Handler: 'hello',
+      Role: 'arn:aws:iam::284327142304:role/serverless-lambda',
+      Runtime: 'provided',
+      Description: 'php lambda',
+      Layers: [
+        'arn:aws:lambda:us-east-1:284327142304:layer:php-73-runtime:1',
+        'arn:aws:lambda:us-east-1:284327142304:layer:php-73-runtime-vendor:1',
+      ],
+      MemorySize: 128,
+      Publish: true,
+      Timeout: 900
+    };
 
 
+    const lambdaFunction = new AWS.Lambda({
+      apiVersion: '2015-03-31',
+      credentials: this.context.credentials.aws,
+      region: 'us-east-1'
+    });
 
+    const response = await lambdaFunction.createFunction(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else console.log(data);           // successful response
+    }).promise();
 
-
+    console.log('RESPONSE: ', response)
 
     return {}
   }
